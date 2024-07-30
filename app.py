8c8e26901ad8e9100abdbb44811bd73a1fa13d60
Sachchit Balamurugan <sachchit.cool@gmail.com>
2024-07-29 23:00:51 -0500

    finished UI for profile, dashboard, settings

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def complete_profile():
+    return render_template('complete_profile.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
