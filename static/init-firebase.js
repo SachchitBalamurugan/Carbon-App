d6f450cf7532bd4aa826bee0e31bfc7b19d1a5c0
Sachchit Balamurugan <sachchit.cool@gmail.com>
2024-09-05 13:46:39 -0500

    FIREBASE WORKSSSSS

---
+++ static/init-firebase.js
@@ -0,0 +1,17 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBT1NH-JfDLGx-UwhX3_JiEx_s1uNNtl-8",
+    authDomain: "carbonapp-4f198.firebaseapp.com",
+    projectId: "carbonapp-4f198",
+    storageBucket: "carbonapp-4f198.appspot.com",
+    messagingSenderId: "581642423590",
+    appId: "1:581642423590:web:2ab9ab765a4567f04d85e7",
+    measurementId: "G-M0FJFDT20X"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
+
